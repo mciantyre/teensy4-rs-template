effe19232ee858802090f37268f535e17d35faef
Ian McIntyre <ianpmcintyre@gmail.com>
2020-01-22 21:58:47 -0500

    First commit

---
+++ hardware-test.bat
@@ -0,0 +1,23 @@
+@echo off
+
+SET scriptbuildflag=--release
+SET scriptbin={{crate_name}}
+IF "%1"=="--debug" (SET "scriptbuildflag=")
+IF NOT DEFINED scriptbuildflag (
+	SET scriptbuildmode=debug
+) ELSE (
+	SET scriptbuildmode=release
+)
+cargo build %scriptbuildflag% || goto :error
+rmdir /S /Q out || goto :error
+mkdir out || goto :error
+copy "target\thumbv7em-none-eabihf\%scriptbuildmode%\%scriptbin%" "out\%scriptbin%" || goto :error
+REM arm-none-eabi-objdump -d -S -C "out\%scriptbin%" > "out\%scriptbin%.lst" || goto :error
+REM arm-none-eabi-objdump -t -C "out\%scriptbin%" > "out\%scriptbin%.sym" || goto :error
+arm-none-eabi-objcopy -O ihex -R .eeprom "out\%scriptbin%" "out\%scriptbin%.hex" || goto :error
+echo Now use Teensy Loader to load out\%scriptbin%.hex onto the Teensy 4.
+goto :EOF
+
+:error
+echo Failed with error code %errorlevel%.
+exit /b %errorlevel%
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt;
+
+use bsp::rt::entry;
+use cortex_m::asm::wfi;
+use teensy4_bsp as bsp;
+
+use embedded_hal::digital::v2::OutputPin;
+
+#[entry]
+fn main() -> ! {
+    let mut peripherals = bsp::Peripherals::take().unwrap();
+    loop {
+        peripherals.led.set_high().unwrap();
+        wfi();
+    }
+}

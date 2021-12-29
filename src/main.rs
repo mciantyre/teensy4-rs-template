556ec0e8e0548173e308a79efc064080b157cc46
Ian McIntyre <ianpmcintyre@gmail.com>
2021-12-29 15:44:11 -0500

    Revise example for 0.3.0 release
    There's no more SYSTICK, so we need another time source. Recommending a
    GPT, since it has OK HAL docs.

--- src/main.rs
+++ src/main.rs
@@ -6,24 +6,66 @@
 
 use teensy4_bsp as bsp;
 use teensy4_panic as _;
+use core::time::Duration;
 
 mod logging;
 
-const LED_PERIOD_MS: u32 = 1_000;
+const LED_PERIOD: Duration = Duration::from_millis(1_000);
+/// The GPT output compare register we're using for
+/// tracking time. This is the first register, since
+/// we're using reset mode.
+const GPT_OCR: bsp::hal::gpt::OutputCompareRegister = bsp::hal::gpt::OutputCompareRegister::One;
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
-    let p = bsp::Peripherals::take().unwrap();
-    let mut systick = bsp::SysTick::new(cortex_m::Peripherals::take().unwrap().SYST);
-    let pins = bsp::t40::into_pins(p.iomuxc);
+    let mut periphs = bsp::Peripherals::take().unwrap();
+
+    // Reduce the number of pins to those specific
+    // to the Teensy 4.0.
+    let pins = bsp::pins::t40::from_pads(periphs.iomuxc);
+    // Prepare the LED, and turn it on!
+    // (If it never turns off, something
+    // bad happened.)
     let mut led = bsp::configure_led(pins.p13);
+    led.set();
+
+    // Prepare the ARM clock to run at ARM_HZ.
+    periphs.ccm.pll1.set_arm_clock(
+        bsp::hal::ccm::PLL1::ARM_HZ,
+        &mut periphs.ccm.handle,
+        &mut periphs.dcdc,
+    );
+
+    // Prepare a GPT timer for blocking delays.
+    let mut timer = {
+        // Run PERCLK on the crystal oscillator (24MHz).
+        let mut cfg = periphs.ccm.perclk.configure(
+            &mut periphs.ccm.handle,
+            bsp::hal::ccm::perclk::PODF::DIVIDE_1,
+            bsp::hal::ccm::perclk::CLKSEL::OSC,
+        );
+
+        let mut gpt1 = periphs.gpt1.clock(&mut cfg);
+        // Keep ticking if we enter wait mode.
+        gpt1.set_wait_mode_enable(true);
+        // When the first output compare register compares,
+        // reset the counter back to zero.
+        gpt1.set_mode(bsp::hal::gpt::Mode::Reset);
+
+        // Compare every LED_PERIOD_US ticks.
+        gpt1.set_output_compare_duration(GPT_OCR, LED_PERIOD);
+        gpt1
+    };
 
     // See the `logging` module docs for more info.
     assert!(logging::init().is_ok());
 
+    timer.set_enable(true);
     loop {
         led.toggle();
-        systick.delay(LED_PERIOD_MS);
         log::info!("Hello world");
+
+        while !timer.output_compare_status(GPT_OCR).is_set() {}
+        timer.output_compare_status(GPT_OCR).clear();
     }
 }
